650cb1630eb8ae28f4fed89b839745feca477e8d
Dzhaokan <ikanat1993@gmail.com>
2024-04-26 18:15:37 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import Navbar from "./components/Navbar";
+import Sidebar from "./components/Sidebar";
+
+function App() {
+  return (
+    <main className="flex">
+      <Sidebar />
+      <div className="flex flex-col flex-1 relative">
+        <Navbar />
+        <div className="grid md:grid-cols-3 grid-cols-1 w-full">
+          <div className="col-span-2">Left Column</div>
+          <div className="w-full">Right Column</div>
+        </div>
+      </div>
+    </main>
+  );
+}
+
+export default App;
